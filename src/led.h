98ac0ecc4441465c7cf0b0274745626165e75b1f
Andreas Schroeder <an.schroeder@kostal.com>
2018-07-14 03:23:04 +0800

    Added comments.

--- src/led.h
+++ src/led.h
@@ -1,7 +1,19 @@
+/**
+ * @file
+ * Header for led control module.
+ */
+
 #ifndef LED_H
 #define LED_H
 
+/**
+ * Initialize the LED.
+ */
 void led_init(void);
+
+/**
+ * Run the led task. This regularly blinks the LED.
+ */
 void led_main(void);
 
 #endif /* LED_H */
