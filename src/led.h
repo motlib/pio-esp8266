537ba44102f9b1cf0a28a21fadb6cc24c0a3907f
Andreas Schroeder <andreas@a-netz.de>
2018-07-22 14:05:21 +0000

    Added comments to led module.

--- src/led.h
+++ src/led.h
@@ -1,7 +1,9 @@
 /**
  * @file
  *
- * Header for led control module.
+ * Header for led control module. Idea is to control a LED to flash fast in a
+ * sequence and than have a longer interval where it's off. This makes it easy
+ * to count the flashes and indicate any kind of status.
  */
 
 #ifndef LED_H
@@ -14,16 +16,30 @@
 
 /**
  * Initialize the LED.
+ *
+ * This initializes the LED i/o port.
+ *
+ * @param[in] led Pointer to the LED configuration / data structure.
  */
 void led_init(led_cfg_t const * led_cfg);
 
 /**
- * Run the led task. This regularly blinks the LED.
- */
+ * Run the led task.
+ *
+ * This regularly flashes the LED, so it must be called with a regular time
+ * interval, e.g. every 10ms.
+ *
+ * @param[in] led Pointer to the LED configuration / data structure.
+*/
 void led_main(led_cfg_t const * led_cfg);
 
 /**
- * Set the state of the led.
+ * Set the flash count state of the led.#
+ *
+ * A flash count of 0 switches off the LED completely.
+ *
+ * @param[in] led Pointer to the LED configuration / data structure.
+ * @param flash_count Number of flashes.
  */
 void led_set_flash_count(led_cfg_t const * led_cfg, uint8_t flash_count);
 
--- src/led_types.h
+++ src/led_types.h
@@ -1,3 +1,9 @@
+/**
+ * @file
+ *
+ * Datatypes to control led operation.
+ */
+
 #ifndef LED_TYPES_H
 #define LED_TYPES_H
 
@@ -6,17 +12,35 @@
  */
 typedef struct
 {
+    /** Number of flashes to show. */
     uint8_t flash_count;
+    /** Current state of the led (led on / off / long delay). */
     uint8_t state;
+
+    /** Time counter to measure how long a state of the led shall be
+     * active. [10ms] */
     uint16_t cnt;
 } led_data_t;
 
+
+/**
+ * Led confoguration structure.
+ */
 typedef struct
 {
+    /** The i/o pin to control (Arduino numbering) */
     uint8_t pin;
+    /** Pointer to the runtime data structure. */
     led_data_t * data;
+
+    /** Time while the led is on [led main function calls] */
     uint16_t flash_on_time;
+
+    /** Time while the led is off [led main function calls] */    
     uint16_t flash_off_time;
+
+    /** Time while the led is off waiting for the next flash cycle [led main
+     * function calls] */
     uint16_t flash_wait_time;
 } led_cfg_t;
 
